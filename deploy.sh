547c8062eb3ad3ccc4096fa9818032a5f697e6cd
Patricia Ortuño <patricia.ortuno28@gmail.com>
2022-11-11 20:42:41 +0100

    Changed paths

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd practica_big_data_2019
 # Required installation for new gnome terminals
 echo "Installing package for gnome terminals ..."
 sudo apt install -y dbus-x11
+sudo apt install -y gnome-terminal
 
 # Install initial requirements
 echo "Installing initial requirements ..."
@@ -33,12 +34,12 @@ echo "Creating a new kafka topic ..."
 
 # Start MongoDB
 echo "Starting mongoDB ..."
-service mongod start
+sudo service mongod start
 
 # Import airline data using monsh
 echo "Importing airline data ..."
-sed -i 's/mongo /mongosh /g' resources/import_distances.sh
-./resources/import_distances.sh
+sed -i 's/mongo /mongosh /g' practica_big_data_2019/resources/import_distances.sh
+./practica_big_data_2019/resources/import_distances.sh
 
 # Export environment variables
 echo "Exporting environment variables ..."
@@ -47,15 +48,15 @@ export SPARK_HOME=/opt/spark
 
 # Train the model
 echo "Training the model ..."
-python3 resources/train_spark_mllib_model.py .
+python3 practica_big_data_2019/resources/train_spark_mllib_model.py practica_big_data_2019
 
 # Modify the base_path
 echo "Creating JAR file ..."
-sed -i 's+val base_path= "/Users/admin/IdeaProjects/ging/practica_big_data_2019"+val base_path= "/home/$USER/practica_big_data_2019"+g' \
-	/home/$USER/practica_big_data_2019/flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
+sed -i 's+val base_path= "/Users/admin/IdeaProjects/ging/practica_big_data_2019"+val base_path= "/home/$USER/PFINAL-FBID2022/practica_big_data_2019"+g' \
+	/home/$USER/PFINAL-FBID2022/practica_big_data_2019/flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
 
 # Create JAR file to use with spark-submit command
-cd flight_prediction
+cd practica_big_data_2019/flight_prediction
 sbt package
 chmod 777 target/scala-2.12/flight_prediction_2.12-0.1.jar
 cd
@@ -63,15 +64,15 @@ cd
 # Spark-submit
 echo "Submitting JAR file to Spark ..."
 gnome-terminal --tab --title="Spark Submit" \
-	--command="bash -c 'cd; /opt/spark/bin/spark-submit --deploy-mode client --class es.upm.dit.ging.predictor.MakePrediction --master local[2] --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/patricia/practica_big_data_2019/flight_prediction/target/scala-2.12/flight_prediction_2.12-0.1.jar; $SHELL'"
+	--command="bash -c 'cd; /opt/spark/bin/spark-submit --deploy-mode client --class es.upm.dit.ging.predictor.MakePrediction --master local[2] --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/$USER/PFINAL-FBID2022/practica_big_data_2019/flight_prediction/target/scala-2.12/flight_prediction_2.12-0.1.jar; $SHELL'"
 
 # Run the web service
 echo "Running the web service ..."
-export PROJECT_HOME=/home/$USER/practica_big_data_2019
+export PROJECT_HOME=/home/$USER/PFINAL-FBID2022/practica_big_data_2019
 gnome-terminal --tab --title="Web Service" \
-	--command="bash -c 'cd /home/$USER/practica_big_data_2019/resources/web; pip install joblib; python3 predict_flask.py; $SHELL'"
+	--command="bash -c 'cd /home/$USER/PFINAL-FBID2022/practica_big_data_2019/resources/web; pip install joblib; python3 predict_flask.py; $SHELL'"
 
 # Show database information
 echo "Showing database information ..."
 gnome-terminal --tab --title="Database Info" \
-	--command="bash -c 'cd /home/$USER/; mongosh agile_data_science showdb.js; $SHELL'"
+	--command="bash -c 'cd /home/$USER/PFINAL-FBID2022; mongosh agile_data_science showdb.js; $SHELL'"
