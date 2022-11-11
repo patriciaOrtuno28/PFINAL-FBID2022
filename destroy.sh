965d1ec6d5567b342276de0a746c3ab578dc3a61
Patricia Ortuño <patricia.ortuno28@gmail.com>
2022-11-11 21:08:34 +0100

    Deleted topic ondestroy

--- destroy.sh
+++ destroy.sh
@@ -6,4 +6,9 @@ rm -Rf practica_big_data_2019
 /usr/local/kafka/bin/kafka-server-stop.sh
 service mongod stop
 
+/usr/local/kafka/bin/kafka-topics.sh \
+      --bootstrap-server localhost:9092 \
+      --delete
+      --topic flight_delay_classification_request
+
 echo "Scenario destroyed successfully!"
