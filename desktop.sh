5122893bd35f233ca4194dfd90902bfdb7e34e53
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-11-13 10:52:36 +0100

    Added NoMachine for Linux instalation inside the desktop automated script

--- desktop.sh
+++ desktop.sh
@@ -2,3 +2,6 @@
 
 sudo apt update && sudo apt upgrade
 sudo apt install -y ubuntu-desktop
+wget https://download.nomachine.com/download/8.2/Linux/nomachine_8.2.3_4_amd64.deb
+sudo apt install ./nomachine_8.2.3_4_amd64.deb
+rm nomachine_8.2.3_4_amd64.deb
