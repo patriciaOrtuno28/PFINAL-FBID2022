298bc61499ac801256a3d5439577032de7fee090
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-11-15 21:45:06 +0100

    Dockerfile for Spark

---
+++ docker/spark/spark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Train the model
+echo "Training the model ..."
+cd practica_big_data_2019
+python3 resources/train_spark_mllib_model.py practica_big_data_2019
+
+# Modify the base_path
+echo "Creating JAR file ..."
+sed -i 's@val base_path= "/Users/admin/IdeaProjects/ging/practica_big_data_2019"@val base_path= "/home/" + sys.env("USER") + "/practica_big_data_2019"@g' \
+        /home/$USER/practica_big_data_2019/flight_prediction/src/main/scala/es/upm/dit/ging/predictor/MakePrediction.scala
+
+# Create JAR file to use with spark-submit command
+cd flight_prediction
+sbt package
+chmod 777 target/scala-2.12/flight_prediction_2.12-0.1.jar
+
