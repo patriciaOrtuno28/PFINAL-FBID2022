836f99adb80dab0de3acaf14ec35402faf0c1fdd
laurafdez <lauraa.fdez24@gmail.com>
2022-11-16 20:06:22 +0100

    instalación de docker-compose

--- VMDocker.sh
+++ VMDocker.sh
@@ -14,5 +14,7 @@ apt-cache policy docker-ce
 
 sudo apt install -y docker-ce
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+sudo chmod +x /usr/local/bin/docker-compose
 
