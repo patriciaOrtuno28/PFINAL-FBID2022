d702ba170460e94eb06c0916e07ccaea4c0b4363
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-11-17 18:48:32 +0100

    Fixed path in prediction for flask to call kafka

--- docker/flask/flask.sh
+++ docker/flask/flask.sh
@@ -2,8 +2,10 @@
 
 cd practica_big_data_2019/resources/web
 
-echo "http://localhost:5000/flights/delays/predict_kafka"
+echo "http://x.x.x.x:5000/flights/delays/predict_kafka"
 
 pip install joblib
+
+sed -i 's@localhost:9092@kafka:9092@g' predict_flask.py
 
 python3 predict_flask.py
