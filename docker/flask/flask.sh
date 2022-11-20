edd49034dbc4d58bcfe3b1b782c36faeb697f362
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-11-20 16:05:16 +0100

    Fixed flask dockerfile

--- docker/flask/flask.sh
+++ docker/flask/flask.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 cd practica_big_data_2019/resources/web
-
-echo "http://x.x.x.x:5000/flights/delays/predict_kafka"
-
-pip install joblib
-
-sed -i 's@localhost:9092@kafka:9092@g' predict_flask.py
-
 python3 predict_flask.py
